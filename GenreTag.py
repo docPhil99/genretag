d5965212106253d2acac52fd6f2385e9d79f354f
Philip Birch <philipbirch@philips-mbp.home>
2017-12-17 18:09:40 +0000

    recursive search option added, more tags defined

--- GenreTag.py
+++ GenreTag.py
@@ -5,40 +5,66 @@ import os
 import sys
 import taglib
 import argparse
-
+import fnmatch
 parser=argparse.ArgumentParser(description="Changes the genre tags of the music in a give directoy. If no options are given it prints the existing tags")
 parser.add_argument('name',help='directory of music to change. ')
 #the optional genre tags
+parser.add_argument('-R','--recursive',action='store_true',help="Recursively search directories")
+
 parser.add_argument('-o','--orchestral',action='store_true')
 parser.add_argument('-c','--chamber',action='store_true')
 parser.add_argument('-p','--opera',action='store_true')
-
+parser.add_argument('-ch','--choral',action='store_true')
+parser.add_argument('-pi','--paino',action='store_true')
 parser.add_argument('-f','--film',action='store_true')
 parser.add_argument('-j','--jazz',action='store_true')
 parser.add_argument('-r','--poprock',action='store_true')
 parser.add_argument('-x','--christmas',action='store_true')
+parser.add_argument('-m','--minimalist',action='store_true')
+parser.add_argument('-E','--early',action='store_true')
+parser.add_argument('-B','--baroque',action='store_true')
+parser.add_argument('-RO','--romantic',action='store_true')
+parser.add_argument('-C','--classical',action='store_true')
+parser.add_argument('-N','--nationalistic',action='store_true')
+parser.add_argument('-MO','--modern',action='store_true')
+parser.add_argument('--misc',action='store_true')
+
 args=vars(parser.parse_args())
 print(args)
 newGenre=[]
 for key,value in args.iteritems():
     if value:
-        if key is not 'name':
-            newGenre.append(key.upper())
+        if key is not 'name' and key is not 'recursive':
+            newGenre.append(key)
 print newGenre
-extList=('*.flac','*.mp3','*.m4a')
+extList=('.flac','.mp3','.m4a')
 print len(sys.argv)
 path=args['name']
 files=[]
-for ext in extList:
-    fs=os.path.join(path,ext)
-    print fs
-    files.append(glob.glob(fs))
-flatten_files=[y for x in files for y in x]
-assert len(flatten_files)>0, "No supported media files found"
+if not args['recursive']:
+    print("Scanning...")
+    for ext in extList:
+        fs=os.path.join(path,'*'+ext)
+        print(fs)
+        files.append(glob.glob(fs))
+        flatten_files=[y for x in files for y in x]
+
+else:
+    for root, dirnames, filenames in os.walk(path):
+        for filename in filenames:
+            if filename.endswith(extList):
+                files.append(os.path.join(root, filename))
+    flatten_files=files
+#assert len(flatten_files)>0, "No supported media files found"
+if len(flatten_files)==0:
+    sys.exit("No supported media file found")
 for file in flatten_files:
     print('File '+file)
     song=taglib.File(file)
-    print(song.tags["GENRE"])
+    if "GENRE" in song.tags.keys():
+         print(song.tags["GENRE"])
+    else:
+         print("No tag")
     if len(newGenre)>0:
         song.tags["GENRE"]=newGenre
         ret=song.save()
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # genretag
 A simple script that changes all the genre tags (via taglib) in a directory. The tags are set via flags on the command line. Run ./GenreTag.py -h for help.
+
+Currently only works in python2.7
+
+You need to install taglib and then pytaglib
+
+On the mac using port
+```
+sudo port install taglib
+```
+```
+pip install pytaglib
+```
+this failed since it could not find the taglib headers so I had to tell pip where to look
+```
+pip install --global-option=build_ext --global-option="-I/opt/local/include/" --global-option="-L/opt/local/lib" pytaglib
+```
