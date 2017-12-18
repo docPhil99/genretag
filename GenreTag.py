a2cb2f7ad492363e152a4a6303059cbcb4b90fda
Philip Birch <dr.phil.birch@gmail.com>
2017-12-18 20:23:21 +0000

    added some try/catches

--- GenreTag.py
+++ GenreTag.py
@@ -1,5 +1,7 @@
 #! /usr/bin/python
 """A simple script that changes all the genre tags (via taglib) in a directory. The tags are set via flags on the command line. Run ./GenreTag -h for help. """
+from __future__ import print_function    # (at top of module)
+
 import glob
 import os
 import sys
@@ -16,7 +18,7 @@ parser.add_argument('-c','--chamber',action='store_true')
 parser.add_argument('-p','--opera',action='store_true')
 parser.add_argument('-ch','--choral',action='store_true')
 parser.add_argument('-pi','--paino',action='store_true')
-parser.add_argument('-f','--film',action='store_true')
+parser.add_argument('-s','--soundtrack',action='store_true')
 parser.add_argument('-j','--jazz',action='store_true')
 parser.add_argument('-r','--poprock',action='store_true')
 parser.add_argument('-x','--christmas',action='store_true')
@@ -36,9 +38,8 @@ for key,value in args.iteritems():
     if value:
         if key is not 'name' and key is not 'recursive':
             newGenre.append(key)
-print newGenre
+print(newGenre)
 extList=('.flac','.mp3','.m4a','.aiff')
-print len(sys.argv)
 path=args['name']
 files=[]
 if not args['recursive']:
@@ -60,12 +61,20 @@ if len(flatten_files)==0:
     sys.exit("No supported media file found")
 for file in flatten_files:
     print('File '+file)
-    song=taglib.File(file)
+    try:
+        song=taglib.File(file)
+    except:
+        print("Could not read file")
+        continue
+    
     if "GENRE" in song.tags.keys():
          print(song.tags["GENRE"])
     else:
          print("No tag")
     if len(newGenre)>0:
-        song.tags["GENRE"]=newGenre
-        ret=song.save()
-        print(ret)
+        try:
+            song.tags["GENRE"]=newGenre
+            ret=song.save()
+            print("Set to new genre ")
+        except:
+            print("Could not save file!")
